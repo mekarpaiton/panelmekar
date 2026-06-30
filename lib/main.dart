df00a118b1003a86b654a6b181bd20c59403468e
mekarpaiton <mekarpaiton@gmail.com>
2026-06-30 09:02:26 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:share_plus/share_plus.dart';
 import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:print_bluetooth_thermal/print_bluetooth_thermal.dart';
+import 'package:esc_pos_utils_flutter/esc_pos_utils_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:image_picker/image_picker.dart';
 
